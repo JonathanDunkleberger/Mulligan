cffb1a31321b51666e3b7852b092760191065b2d
Jonny <jonny.dunk52@gmail.com>
2025-11-22 14:48:15 -0700

    Add i.ytimg.com to next.config.js for video thumbnails

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
       { protocol: "https", hostname: "images.igdb.com" },
+      { protocol: "https", hostname: "i.ytimg.com" },
       // Google Books sometimes serves from books.google.com or books.googleusercontent.com
       { protocol: "https", hostname: "books.google.com" },
       { protocol: "https", hostname: "books.googleusercontent.com" }
